dea27038f81c1ac00295b393be8978ae3d436a5e
mikeycha <32110940+mikeycha@users.noreply.github.com>
2020-03-12 17:20:40 +0000

    added management class in respect to: MANAGEMENT: minimum payable is 3.00

---
+++ main/java/ManagementCalculate.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class ManagementCalculate implements ICalculateRates {
+
+    @Override
+    public BigDecimal calculateReduction(BigDecimal rate) {
+        BigDecimal manageMinPay = new BigDecimal("3");
+        if (rate.compareTo(manageMinPay) == -1)  return manageMinPay;
+        else return rate;
+    }
+
+}
