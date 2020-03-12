a54e317cbba42ecfac75e5433de4564701757fb9
mikeycha <32110940+mikeycha@users.noreply.github.com>
2020-03-12 17:11:41 +0000

    added visitor class in respect to: VISITOR: first 8.00 is free, 50% reduction above that e.g total calculated cost was 15.00, after reduction only 3.50 needs to be paid (i.e. 50% of (15.00-8.00))

---
+++ main/java/VisitorCalculate.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class VisitorCalculate implements ICalculateRates {
+
+    @Override
+    public BigDecimal calculateReduction(BigDecimal rate) {
+        BigDecimal visitorFree = new BigDecimal("8");
+        BigDecimal visitorReduction = new BigDecimal("0.50");
+        if (rate.compareTo(visitorFree) == -1) return BigDecimal.ZERO;
+        else return ((rate.subtract(visitorFree)).multiply(visitorReduction));
+    }
+}
